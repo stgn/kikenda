13f12efa0730f61e1f497a1008be0d9e5e2cca24
Shane Nelson <181157+stgn@users.noreply.github.com>
2025-09-27 19:08:13 -0400

    Pipeline work

--- src/kikenda/pipeline.py
+++ src/kikenda/pipeline.py
@@ -1,4 +1,4 @@
-from typing import Protocol, overload
+from typing import Any, Protocol, cast
 
 
 class Stage[A, B](Protocol):
@@ -6,59 +6,45 @@ class Stage[A, B](Protocol):
     def inverse(self, data: B) -> A: ...
 
 
-class _EmptyPipeline:
-    def then[A, B](self, next_: Stage[A, B]) -> "Pipeline[A, B]":
-        return Pipeline(next_)
+class _Identity[A](Stage[A, A]):
+    def forward(self, data: A) -> A:
+        return data
 
-    def __or__[A, B](self, next_: Stage[A, B]) -> "Pipeline[A, B]":
-        return self.then(next_)
+    def inverse(self, data: A) -> A:
+        return data
+
+    def __getitem__[B](self, ty: type[B]) -> "_Identity[B]":
+        return cast("_Identity[B]", self)
 
 
-EmptyPipeline = _EmptyPipeline()
+Identity = _Identity[Any]()
+
+
+class Parallel[LA, LB, RA, RB](Stage[tuple[LA, RA], tuple[LB, RB]]):
+    __slots__ = "_left", "_right"
+
+    def __init__(self, left: Stage[LA, LB], right: Stage[RA, RB]):
+        self._left = left
+        self._right = right
+
+    def forward(self, data: tuple[LA, RA]) -> tuple[LB, RB]:
+        ld, rd = data
+        return self._left.forward(ld), self._right.forward(rd)
+
+    def inverse(self, data: tuple[LB, RB]) -> tuple[LA, RA]:
+        ld, rd = data
+        return self._left.inverse(ld), self._right.inverse(rd)
 
 
 class Composed[A, B, C](Stage[A, C]):
-    __slots__ = "prev", "next"
+    __slots__ = "_prev", "_next"
 
     def __init__(self, prev: Stage[A, B], next_: Stage[B, C]):
-        self.prev = prev
-        self.next = next_
+        self._prev = prev
+        self._next = next_
 
     def forward(self, data: A) -> C:
-        return self.next.forward(self.prev.forward(data))
+        return self._next.forward(self._prev.forward(data))
 
     def inverse(self, data: C) -> A:
-        return self.prev.inverse(self.next.inverse(data))
-
-
-class Pipeline[A, B](Stage[A, B]):
-    __slots__ = ("_stage",)
-
-    @overload
-    def __new__(cls) -> _EmptyPipeline: ...
-
-    @overload
-    def __new__(cls, stage: Stage[A, B]) -> "Pipeline[A, B]": ...
-
-    def __new__(
-        cls, stage: Stage[A, B] | None = None
-    ) -> "Pipeline[A, B] | _EmptyPipeline":
-        if stage is None:
-            return EmptyPipeline
-        return super().__new__(cls)
-
-    def __init__(self, stage: Stage[A, B] | None = None):
-        assert stage is not None
-        self._stage = stage
-
-    def forward(self, data: A) -> B:
-        return self._stage.forward(data)
-
-    def inverse(self, data: B) -> A:
-        return self._stage.inverse(data)
-
-    def then[C](self, next_: Stage[B, C]) -> "Pipeline[A, C]":
-        return Pipeline(Composed(self._stage, next_))
-
-    def __or__[C](self, next_: Stage[B, C]) -> "Pipeline[A, C]":
-        return self.then(next_)
+        return self._prev.inverse(self._next.inverse(data))
